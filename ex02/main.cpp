c383a7cf8d17f40b8bf852c809e254f63effa386
Christian Wolf <cwolf@1-A-29.42heilbronn.de>
2025-07-17 14:10:53 +0200

    changed main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cwolf <cwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 16:46:43 by cwolf             #+#    #+#             */
-/*   Updated: 2025/07/09 14:58:53 by cwolf            ###   ########.fr       */
+/*   Updated: 2025/07/17 13:58:05 by cwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main()
 	std::string &stringREF = brain;
 	
 	std::cout << "Memory address of the string variable: " <<&brain <<std::endl;
-	std::cout << "Memory address held by stringPTR:     " <<&stringPTR <<std::endl;
+	std::cout << "Memory address held by stringPTR:     " <<stringPTR <<std::endl;
 	std::cout << "Memory address held by stringREF:     " <<&stringREF <<std::endl;
 	
 	std::cout << "Value of the string variable: " <<brain <<std::endl;
